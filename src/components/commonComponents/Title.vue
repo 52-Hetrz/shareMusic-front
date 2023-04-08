03319357e3387f40d23205ce09387210b9da2918
life <1219821955@qq.com>
2023-04-08 22:54:53 +0800

    修改title样式

--- src/components/commonComponents/Title.vue
+++ src/components/commonComponents/Title.vue
@@ -1,27 +1,33 @@
 <template>
-  <div style="height: 100%;width: 100%;font-weight: bold;font-size: 1rem">
+  <div style="height: 100%;width: 100%;font-weight: bold;font-size: 1rem;">
     <el-row >
       <el-col :span="14">
-        <span>Share Music</span>
+        <div class="title1-color">Share Music</div>
       </el-col>
     </el-row>
     <el-row>
       <el-col :offset="8" :span="14">
-        <span>Share Life</span>
+        <div class="title2-color">Share Life</div>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-window.onresize = function (){
-  console.log("asdf")
-}
 export default {
   name: "Title",
 }
 </script>
 
 <style scoped>
-
+  .title1-color {
+    background-image: linear-gradient(to right, rgba(118, 220, 59, 0.8), rgba(49, 126, 5, 0.8));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  .title2-color {
+    background-image: linear-gradient(to right, rgba(49, 126, 5, 0.8), rgba(7, 132, 249, 0.8));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
 </style>
